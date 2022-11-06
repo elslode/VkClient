3018919bd6c7ca4fbb66826aeed497d0b642f65b
elsloude <elisey.vavulin15@gmail.com>
2022-11-06 14:28:24 +0100

    code multiplication table

--- app/src/main/java/com/bibliographer/vkclient/MainActivity.kt
+++ app/src/main/java/com/bibliographer/vkclient/MainActivity.kt
@@ -3,37 +3,53 @@ package com.bibliographer.vkclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.bibliographer.vkclient.ui.theme.VkClientTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UserInfo(name =  "Елисей Вавулин", age =  24)
+            TimesTable()
         }
-
     }
 }
 
 @Preview
 @Composable
-fun Greeting() {
-    UserInfo(name = "Jhon", age = 121)
-}
-
-@Composable
-fun UserInfo(name: String, age: Int) {
-    Column {
-        for(i in 0 until 10) {
-            Text(text = "Hello, $name, you are $age age")
+fun TimesTable() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        for (i in 1 until 10) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                for (j in 1 until 10) {
+                    val color = if ((i + j) % 2 == 0) Color.Green else Color.White
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f)
+                            .border(width = 1.dp, color = Color.Black)
+                            .background(color),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "${i * j}")
+                    }
+                }
+            }
         }
     }
 }
