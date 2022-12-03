2e2c62704af4222f2f272c4f6939fb9568aa840e
elsloude <elisey.vavulin15@gmail.com>
2022-12-03 23:02:18 +0100

    add scaffold composable element

--- app/src/main/java/com/bibliographer/vkclient/MainActivity.kt
+++ app/src/main/java/com/bibliographer/vkclient/MainActivity.kt
@@ -5,13 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,9 +33,61 @@ class MainActivity : ComponentActivity() {
                         .background(MaterialTheme.colors.background)
                         .padding(8.dp)
                 ) {
-                    PostCard()
+                    Test2()
                 }
             }
+        }
+    }
+
+    @Composable
+    private fun VkPostCard() = PostCard()
+
+    @Preview
+    @Composable
+    private fun Test2() {
+        val selectedItem by remember { mutableStateOf(0) }
+        val items = listOf("Songs", "Artist", "Playlist")
+
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = "TopAppBarTitile")
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {}) {
+                            Icon(Icons.Filled.Menu, contentDescription = null)
+                        }
+                    }
+                )
+            },
+            bottomBar = {
+                BottomNavigation {
+                    items.forEachIndexed { index, item ->
+                        BottomNavigationItem(
+                            selected = selectedItem == index,
+                            onClick = { /*TODO*/ },
+                            label = { Text(item) },
+                            icon = {
+                                Icon(
+                                    imageVector = Icons.Filled.Favorite,
+                                    contentDescription = null
+                                )
+                            }
+                        )
+                    }
+                }
+            },
+            drawerContent = {
+                Text(text = "First text")
+                Text(text = "Second text")
+                Text(text = "Thirthd text")
+            }
+        ) {
+            Text(
+                modifier = Modifier.padding(20.dp),
+                text = "This is scaffold content"
+            )
         }
     }
 }
