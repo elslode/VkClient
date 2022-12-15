654baa4fe0aee9fde3c2276027776343bdda038f
elsloude <elisey.vavulin15@gmail.com>
2022-12-15 22:16:47 +0100

    snackBar and statr

--- app/src/main/java/com/bibliographer/vkclient/ui/theme/VkNewsMainScreen.kt
+++ app/src/main/java/com/bibliographer/vkclient/ui/theme/VkNewsMainScreen.kt
@@ -1,16 +1,49 @@
 package com.bibliographer.vkclient.ui.theme
 
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
+import java.time.Duration
 
 @Composable
 fun MainScreen() {
+    val snackBarHostState = SnackbarHostState()
+    val scope = rememberCoroutineScope()
+    val fabIsVisible = remember { mutableStateOf(true) }
+
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackBarHostState)
+        },
+        floatingActionButton = {
+            if (fabIsVisible.value) {
+                FloatingActionButton(onClick = {
+                    scope.launch {
+                        val action = snackBarHostState.showSnackbar(
+                            message = "This is snackbar",
+                            duration = SnackbarDuration.Long,
+                            actionLabel = "Hide FAB"
+                        )
+                        if (action == SnackbarResult.ActionPerformed) {
+                            fabIsVisible.value = false
+                        }
+                    }
+                })
+                {
+                    Icon(
+                        imageVector = Icons.Outlined.Favorite,
+                        contentDescription = null
+                    )
+                }
+            }
+
+        },
         bottomBar = {
             BottomNavigation {
                 val selectedItemPosition = remember { mutableStateOf(0) }
